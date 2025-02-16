4a72ac4ed13c09f83fde08d160e88212527aa760
Joel Woeste <woestejoel321@gmail.com>
2025-02-16 18:03:01 -0500

    Committing app.json and index.js

--- client/app/index.js
+++ client/app/index.js
@@ -1,5 +1,6 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useEffect } from "react";
+import { Text, Platform, StatusBar } from "react-native";
+import * as NavigationBar from "expo-navigation-bar";
 
 // Navigation
 import AppNavigator from "./navigation/AppNavigator";
@@ -11,6 +12,18 @@ import { useGlobalFonts } from "./styles/fonts";
 const App = () => {
   const { initialized, isLoggedin } = AuthStore.useState();
   const { fontsLoaded, fontError } = useGlobalFonts();
+  
+
+  // Apply a transparent navigation bar on Android
+  useEffect(() => {
+    if (Platform.OS === "android") {
+      NavigationBar.setVisibilityAsync("hidden"); // Hide navigation bar
+      NavigationBar.setBackgroundColorAsync("transparent"); // Fully transparent
+      NavigationBar.setBehaviorAsync("inset-swipe"); // Keeps navigation gestures
+      NavigationBar.setPositionAsync("absolute"); // Forces it off-screen
+      StatusBar.setHidden(true); // Hide status bar for full immersion
+    }
+  }, []);
 
   if (!fontsLoaded && !fontError) return <Text>Error Loading Fonts!</Text>;
 
